e512054712051238ab9ed8040fdb76718552227c
ervalsa <ervalsa-san@gmail.com>
2022-07-22 13:47:19 +0800

    Add Hero Repository

---
+++ src/main/kotlin/com/ervalsa/repository/HeroRepository.kt
@@ -0,0 +1,19 @@
+package com.ervalsa.repository
+
+import com.ervalsa.models.ApiResponse
+import com.ervalsa.models.Hero
+
+interface HeroRepository {
+
+    val heroes: Map<Int, List<Hero>>
+
+    val page1: List<Hero>
+    val page2: List<Hero>
+    val page3: List<Hero>
+    val page4: List<Hero>
+    val page5: List<Hero>
+
+    suspend fun getAllHeroes(page: Int = 1): ApiResponse
+
+    suspend fun searchHeroes(name: String): ApiResponse
+}
