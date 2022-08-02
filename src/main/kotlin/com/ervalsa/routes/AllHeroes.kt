7f826bfb401e56cd5c6b925b98473bf893c74098
ervalsa <ervalsa-san@gmail.com>
2022-08-02 16:54:33 +0800

    Implement getAllHeroes

--- src/main/kotlin/com/ervalsa/routes/AllHeroes.kt
+++ src/main/kotlin/com/ervalsa/routes/AllHeroes.kt
@@ -1,19 +1,27 @@
 package com.ervalsa.routes
 
 import com.ervalsa.models.ApiResponse
+import com.ervalsa.repository.HeroRepository
 import io.ktor.application.*
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import org.koin.ktor.ext.inject
 import java.lang.NumberFormatException
 
 fun Route.getAllHeroes() {
+    val heroRepository: HeroRepository by inject()
+
     get("/boruto/heroes") {
         try {
             val page = call.request.queryParameters["page"]?.toInt() ?: 1
             require(page in 1..5)
 
-            call.respond(message = page)
+            val apiResponse = heroRepository.getAllHeroes(page = page)
+            call.respond(
+                message = apiResponse,
+                status = HttpStatusCode.OK
+            )
         } catch (e: NumberFormatException) {
             call.respond(
                 message = ApiResponse(success = false, message = "Only Numbers Allowed."),
@@ -22,7 +30,7 @@ fun Route.getAllHeroes() {
         } catch (e: IllegalArgumentException) {
             call.respond(
                 message = ApiResponse(success = false, message = "Heroes Not Found."),
-                status =  HttpStatusCode.BadRequest
+                status =  HttpStatusCode.NotFound
             )
         }
     }
